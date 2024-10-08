b3f13576a89a86ebbe284d9bb3e20954f8a55f7c
NatanaelPLHN <natannaelprimuslung@gmail.com>
2024-10-08 09:00:35 +0800

    review 5 update delete search working

--- pertemuan2/urls.py
+++ pertemuan2/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', views.homepage),
     path('student/', views.student_index, name='student_index'),
     path('student/create/', views.student_create, name='student_create'),# Create
+    path('student/update/<int:student_id>/', views.student_update, name='student_update'),
+    path('student/delete/<int:student_id>/', views.student_delete, name='student_delete'),
 ]
--- pertemuan2/views.py
+++ pertemuan2/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,  get_object_or_404
+from django.contrib import messages
 from .models import Students
 from .models import students
-
-from django.shortcuts import render, redirect
-from django.contrib import messages
 from .forms import StudentsForm
+from django.http import JsonResponse
+from django.db.models import Q
+
 
 # Create your views here.
 def homepage(request):
@@ -29,3 +30,38 @@ def student_create(request):
     else:
         form = StudentsForm()
     return render(request, 'student/create.html', {'form': form})
+
+# UPDATE Mahasiswa
+def student_update(request, student_id):
+    student = get_object_or_404(Students, id=student_id)
+    if request.method == 'POST':
+        form = StudentsForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Data mahasiswa berhasil diubah!')
+            return redirect('pertemuan2:student_index')
+    else:
+        form = StudentsForm(instance=student)
+        return render(request, 'student/update.html', {'form': form, 'student': student})
+    
+# DELETE Mahasiswa
+def student_delete(request, student_id):
+    student = get_object_or_404(Students, id=student_id)
+    student.delete()
+    messages.success(request, 'Data mahasiswa berhasil dihapus')
+    return JsonResponse({'success': True})
+
+#search
+def student_index(request):
+    query = request.GET.get('q')
+    students = Students.objects.all()
+    if query:   
+        students = Students.objects.filter(
+            Q(name__icontains=query) |
+            Q(nim__icontains=query) |
+            Q(email__icontains=query) |
+            Q(phone_number__icontains=query)
+        )
+    else:
+        students = Students.objects.all()
+    return render(request, 'student/index.html', {'students': students, 'query': query})
